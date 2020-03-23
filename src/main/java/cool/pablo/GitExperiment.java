c6c6aa4339a64a1adafb33449c4094238980962b
Pablo Blazquez Lopez <pablobl72@gmail.com>
2020-03-23 14:48:20 +0100

    'README.md' and '.gitignore' added.

--- src/main/java/cool/pablo/GitExperiment.java
+++ src/main/java/cool/pablo/GitExperiment.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class GitExperiment {
+
   public static void main(String[] args) {
     SpringApplication.run(GitExperiment.class, args).close();
   }
