76da6e32effe2321de7dea9da929c5726a516a1b
Pablo Blazquez Lopez <pablobl72@gmail.com>
2020-03-23 14:17:37 +0100

    Initial project version.

---
+++ src/main/java/cool/pablo/GitExperiment.java
@@ -0,0 +1,12 @@
+package cool.pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GitExperiment {
+  public static void main(String[] args) {
+    SpringApplication.run(GitExperiment.class, args).close();
+  }
+}
